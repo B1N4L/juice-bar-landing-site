407ba229e3c837232276a5dc78c0b1c87de940b8
B1N4L <163591915+B1N4L@users.noreply.github.com>
2026-04-27 23:48:00 +0530

    add(Contact): create Contact component and integrate into App layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Cocktails from "./components/Cocktails.jsx";
 import About from "./components/About.jsx";
 import Art from "./components/Art.jsx";
 import Menu from "./components/Menu.jsx";
+import Contact from "./components/Contact.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -18,7 +19,8 @@ function App() {
       <Cocktails/>
       <About/>
       <Art/>
-        <Menu/>
+      <Menu/>
+      <Contact/>
     </main>
   )
 }
